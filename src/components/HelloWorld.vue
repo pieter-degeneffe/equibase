faef08f70ea2ac73fe0d2d3c946410bfc25ea5cd
Pieter-degeneffe <pieterdegeneffe@outlook.com>
2019-11-08 09:47:24 +0100

    Update to test automated build trigger

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,9 +18,7 @@
           Welcome to Equibase
         </h1>
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+          Horse managment app for equibase
         </p>
       </v-flex>
 
