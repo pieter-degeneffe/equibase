da0439daa4535ffb18e231d27003553d47a1e534
Pieter-degeneffe <pieterdegeneffe@outlook.com>
2019-11-08 16:02:43 +0100

    added Auth0 authentication

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import './plugins/axios'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import { Auth0Plugin } from "./auth";
+import { domain, clientId } from "../auth_config.json";
 
 Vue.config.productionTip = false
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
 new Vue({
   vuetify,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Check that the SDK client is not currently loading before accessing is methods -->
+    <div v-if="!$auth.loading">
+      <!-- show login when not authenticated -->
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <!-- show logout when authenticated -->
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
   </div>
 </template>
 
@@ -13,6 +20,15 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout();
+      this.$router.push({ path: "/" });
+    }
   }
 }
 </script>
