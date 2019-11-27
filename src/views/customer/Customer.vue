85f54a282460e9788485263e32a0c452a2cd9e65
Pieter-degeneffe <pieterdegeneffe@outlook.com>
2019-11-27 21:26:49 +0100

    Design improvements

--- src/views/customer/Customer.vue
+++ src/views/customer/Customer.vue
@@ -50,77 +50,88 @@
     </v-tabs>
   </v-card> -->
   <v-card class="ma-5" outlined>
-      <v-toolbar flat color="primary" dark>
-        <v-toolbar-title>{{ customer.first_name }} {{ customer.last_name }}</v-toolbar-title>
-      </v-toolbar>
-      <v-tabs>
-        <v-tab>
-          <v-icon left>mdi-account</v-icon>
-          Persoonlijke gegevens
-        </v-tab>
-        <v-tab>
-          <v-icon left>mdi-horseshoe</v-icon>
-          Paarden
-        </v-tab>
-        <v-tab-item>
-          <v-card flat>
-            <v-form>
-              <v-container fluid>
-                <v-row>
-                  <v-col cols="12" md="4" sm="6">
-                    <v-text-field v-model="customer.first_name" :counter="128" label="Voornaam" required outlined></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="4" sm="6">
-                    <v-text-field v-model="customer.last_name" :counter="128" label="Achternaam" required outlined></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="4" sm="6">
-                    <v-select v-model="customer.language" :items="languages" label="Taal" outlined></v-select>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="12" md="4" sm="6">
-                    <v-text-field v-model="customer.email" :counter="128" label="E-mail adres" required outlined></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="4" sm="6">
-                    <v-text-field v-model="customer.telehpone" :counter="64" label="Telefoonnummer" outlined></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-divider></v-divider>
-                <v-row>
-                  <v-col cols="12" md="3" sm="6">
-                    <v-text-field v-model="customer.straatnaam" :counter="128" label="Straatnaam" outlined></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="3" sm="6">
-                    <v-text-field v-model="customer.house_number" :counter="16" label="Huisnummer" outlined></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="3" sm="6">
-                    <v-text-field v-model="customer.straatnaam" :counter="64" label="Postcode" outlined></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="3" sm="6">
-                    <v-text-field v-model="customer.house_number" :counter="16" label="Gemeente" outlined></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-divider></v-divider>
-                <v-row>
-                  <v-col cols="12" md="4">
-                    <v-btn color="success" class="mr-4" @click="saveCustomer()" depressed>
-                      Klant Opslaan
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-form>
-          </v-card>
-        </v-tab-item>
-        <v-tab-item>
-          <v-card flat>
-            <v-card-text>
-              <p>Hier komt een overzicht van de paarden</p>
-            </v-card-text>
-          </v-card>
-        </v-tab-item>
-      </v-tabs>
-    </v-card>
+    <v-toolbar flat color="primary" dark>
+      <v-toolbar-title>{{ customer.first_name | capitalize }} {{ customer.last_name | capitalize }}</v-toolbar-title>
+    </v-toolbar>
+    <v-tabs>
+      <v-tab>
+        <v-icon left>mdi-account</v-icon>
+        Persoonlijke gegevens
+      </v-tab>
+      <v-tab>
+        <v-icon left>mdi-horseshoe</v-icon>
+        Paarden
+      </v-tab>
+      <v-tab-item>
+        <v-card flat>
+          <v-form>
+            <v-container fluid>
+              <v-row>
+                <v-col cols="12" md="4" sm="6">
+                  <v-text-field v-model="customer.first_name" :counter="128" label="Voornaam" required outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="4" sm="6">
+                  <v-text-field v-model="customer.last_name" :counter="128" label="Achternaam" required outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="4" sm="6">
+                  <v-select v-model="customer.language" :items="languages" label="Taal" outlined></v-select>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" md="4" sm="6">
+                  <v-text-field v-model="customer.email" :counter="128" label="E-mail adres" required outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="4" sm="6">
+                  <v-text-field v-model="customer.telehpone" :counter="64" label="Telefoonnummer" outlined></v-text-field>
+                </v-col>
+              </v-row>
+              <v-divider></v-divider>
+              <v-row>
+                <v-col cols="12" md="3" sm="6">
+                  <v-text-field v-model="customer.straatnaam" :counter="128" label="Straatnaam" outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="3" sm="6">
+                  <v-text-field v-model="customer.house_number" :counter="16" label="Huisnummer" outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="3" sm="6">
+                  <v-text-field v-model="customer.straatnaam" :counter="64" label="Postcode" outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="3" sm="6">
+                  <v-text-field v-model="customer.house_number" :counter="16" label="Gemeente" outlined></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-btn color="success" depressed class="mr-4" @click="saveCustomer()">
+                  Klant opslaan
+                </v-btn>
+                <v-btn color="warning" depressed @click="deleteDialog = true">
+                  Klant verwijderen
+                </v-btn>
+                <v-dialog v-model="deleteDialog" persistent max-width="350">
+                  <v-card>
+                    <v-card-title class="headline">Klant verwijderen?</v-card-title>
+                    <v-card-text>Ben je zeker dat je de klant {{ customer.first_name }} {{ customer.last_name }} wilt verwijderen? Dit kan niet meer ongedaan gemaakt worden</v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="success" depressed @click="deleteDialog = false">Annuleren</v-btn>
+                      <v-btn color="error" depressed @click="deleteCustomer()">Verwijderen</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <p>Hier komt een overzicht van de paarden</p>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
 </template>
 
 <script>
@@ -130,7 +141,8 @@ export default {
   data() {
     return {
       customer: "",
-      languages: ["NL", "FR", "EN"]
+      languages: ["NL", "FR", "EN"],
+      deleteDialog: false
     };
   },
   mounted() {
@@ -147,10 +159,8 @@ export default {
     },
     async deleteCustomer() {
       await customerAPI.deleteCustomer(this.customer._id);
-      this.$router.push({ path: '/customer' })
-    },
-    onEnlargeText: function (enlargeAmount) {
-      this.postFontSize += enlargeAmount
+      this.$router.push({ path: '/customer' });
+      this.dialog = false;
     }
   }
 };
