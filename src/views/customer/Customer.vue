ae5ea3adb9d3a7f96ca9f738a1b0cb3d39fdbcd4
Pieter-degeneffe <pieterdegeneffe@outlook.com>
2019-11-28 20:50:01 +0100

    Removed component code from view

--- src/views/customer/Customer.vue
+++ src/views/customer/Customer.vue
@@ -14,78 +14,12 @@
       </v-tab>
       <v-tab-item class="ma-5">
         <v-card flat>
-          <customerForm :customer="customer"></customerForm>
-          <!-- <v-form ref="form" v-model="valid">
-            <v-container fluid>
-              <v-row>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field v-model="customer.first_name" :counter="64" :rules="nameRules" label="Voornaam" required outlined></v-text-field>
-                </v-col>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field v-model="customer.last_name" :counter="64" :rules="nameRules" label="Achternaam" required outlined></v-text-field>
-                </v-col>
-                <v-col cols="12" md="4" sm="6">
-                  <v-select v-model="customer.language" :rules="required" :items="languages" label="Taal" outlined></v-select>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field v-model="customer.email" :counter="64" :rules="emailRules" label="E-mail adres" required outlined></v-text-field>
-                </v-col>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field v-model="customer.telehpone" :counter="16" :rules="phoneRules" label="Telefoonnummer" persistent-hint hint="voorbeeld: +32486688502" outlined></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field v-model="customer.straatnaam" :counter="32" :rules="length32" label="Straatnaam" outlined></v-text-field>
-                </v-col>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field v-model="customer.house_number" :counter="16" :rules="length16" label="Huisnummer" outlined></v-text-field>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field v-model="customer.zip" :counter="8" :rules="length8" label="Postcode" outlined></v-text-field>
-                </v-col>
-                <v-col cols="12" md="4" sm="6">
-                  <v-text-field v-model="customer.city" :counter="32" :rules="length32" label="Gemeente" outlined></v-text-field>
-                </v-col>
-                <v-col cols="12" md="4" sm="6">
-                  <v-select v-model="customer.country" :items="countries" :rules="required" label="Land" outlined></v-select>
-                </v-col>
-              </v-row>
-              <v-row justify="end">
-                  <v-btn v-if="!customer._id" :disabled="!valid" color="success" depressed class="mr-4" @click="createCustomer()">
-                    Klant opslaan
-                  </v-btn>
-                  <v-btn v-if="customer._id" color="success" depressed class="mr-4" @click="updateCustomer()">
-                    Klant bijwerken
-                  </v-btn>
-                  <v-btn v-if="customer._id" color="warning" depressed @click="deleteDialog = true">
-                    Klant verwijderen
-                  </v-btn>
-                  <v-dialog v-model="deleteDialog" persistent max-width="350">
-                    <v-card>
-                      <v-card-title class="headline">Klant verwijderen?</v-card-title>
-                      <v-card-text>Ben je zeker dat je de klant {{ customer.first_name }} {{ customer.last_name }} wilt verwijderen? Dit kan niet meer ongedaan gemaakt worden</v-card-text>
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="success" depressed @click="deleteDialog = false">Annuleren</v-btn>
-                        <v-btn color="error" depressed @click="deleteCustomer()">Verwijderen</v-btn>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
-              </v-row>
-            </v-container>
-          </v-form> -->
+          <customer-form :customer="customer"></customer-form>
         </v-card>
       </v-tab-item>
       <v-tab-item class="ma-5">
         <v-card flat>
-          <v-card-text>
-            <p>Hier komt een overzicht van de paarden</p>
-          </v-card-text>
+          <customer-horses :customer="customer"></customer-horses>
         </v-card>
       </v-tab-item>
     </v-tabs>
@@ -94,7 +28,8 @@
 
 <script>
 import customerAPI from "@/services/CustomerAPI.js";
-import customerForm from "@/components/CustomerForm";
+import CustomerForm from "@/components/CustomerForm";
+import CustomerHorses from "@/components/CustomerHorses";
 export default {
   props: ["id"],
   data() {
@@ -113,7 +48,8 @@ export default {
     },
   },
   components: {
-    customerForm
+    CustomerForm,
+    CustomerHorses
   },
 };
 </script>
