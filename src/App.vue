2b27f3e29a6004dad0306fb896823d027b12a9b4
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-04 07:41:48 +0100

    Added v-content wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <app-navigation></app-navigation>
-    <router-view></router-view>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
   </v-app>
 </template>
 
