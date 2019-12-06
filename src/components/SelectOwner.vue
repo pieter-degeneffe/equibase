91c4cdd3c9029821e1789c48881e5104f3ecc556
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-06 15:30:37 +0100

    Creqted select owner component

---
+++ src/components/SelectOwner.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <v-autocomplete
+      v-model="model"
+      :items="items"
+      :loading="isLoading"
+      :placeholder="fullName | capitalize"
+      :search-input.sync="search"
+      hide-no-data
+      hide-selected
+      item-text="Description"
+      item-value="_id"
+      label="Eigenaar"
+      return-object
+      outlined
+    ></v-autocomplete>
+  </div>
+</template>
+<script>
+// import customerAPI from "@/services/CustomerAPI.js";
+export default {
+  props: ['owner'],
+  data() {
+    return {
+      descriptionLimit: 60,
+      entries: [],
+      isLoading: false,
+      model: null,
+      search: null,
+    };
+  },
+  computed: {
+    // fields () {
+    //   if (!this.model) return []
+    //   console.log(this.model);
+    //   this.$emit('update-owner', this.model);
+    //   return Object.keys(this.model).map(key => {
+    //     return {
+    //       key,
+    //       value: this.model[key] || 'n/a',
+    //     }
+    //   })
+    // },
+    items () {
+      return this.entries.map(entry => {
+        const Description = entry.first_name.length > this.descriptionLimit
+          ? entry.first_name.slice(0, this.descriptionLimit) + '...'
+          : entry.first_name
+        return Object.assign({}, entry, { Description })
+      })
+    },
+    fullName () {
+      if (this.owner.first_name && this.owner.last_name){
+        return this.owner.first_name + ' ' + this.owner.last_name;
+      }
+      return "Eigenaar";
+    }
+  },
+
+  watch: {
+    search () {
+      // Items have already been loaded
+      if (this.items.length > 0) return
+
+      // Items have already been requested
+      if (this.isLoading) return
+
+      this.isLoading = true
+
+      // Lazily load input items
+      fetch('http://localhost:8081/api/customer')
+        .then(res => res.json())
+        .then(res => {
+          this.entries = res
+        })
+        .catch(err => {
+          console.log(err)
+        })
+        .finally(() => (this.isLoading = false))
+    },
+    model() {
+      this.$emit('update-owner', this.model);
+    }
+  },
+}
+</script>
