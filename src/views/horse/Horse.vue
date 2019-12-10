43575a91000e108c5592b1cae5e9377744759c2e
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-10 21:48:14 +0100

    Added tab for passport

--- src/views/horse/Horse.vue
+++ src/views/horse/Horse.vue
@@ -1,78 +1,100 @@
 <template>
   <v-card class="ma-5" outlined>
-    <v-toolbar flat dark>
+    <v-toolbar flat color="primary" dark>
       <v-toolbar-title>{{ horse.name }}</v-toolbar-title>
     </v-toolbar>
-    <v-form ref="form" v-model="valid">
-      <v-container>
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field v-model="horse.name" :counter="64" :rules="nameRules" label="Naam paard" outlined></v-text-field>
-          </v-col>
-          <v-col cols="12" md="4">
-            <v-select v-model="horse.type" :items="horseType" :rules="required" label="Geslacht" outlined></v-select>
-          </v-col>
-          <v-col cols="12" md="4">
-            <select-owner :owner="owner" @update-owner="updateOwner"></select-owner>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field v-model="horse.ueln" type="number" :counter="15" label="UELN" required outlined></v-text-field>
-          </v-col>
-          <v-col cols="12" md="4" v-if="horse.type === 'merrie'">
-            <v-switch v-model="horse.surrogate" label="Draagmoeder"></v-switch>
-          </v-col>
-          <v-col cols="12" md="4">
-          <v-menu v-model="birthDateMenu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y min-width="290px">
-            <template v-slot:activator="{ on }">
-              <v-text-field v-model="computedBirthDateFormatted" label="Geboortedatum" v-on="on" readonly outlined></v-text-field>
-            </template>
-            <v-date-picker v-model="horse.date_of_birth" no-title @input="birthDateMenu = false"></v-date-picker>
-          </v-menu>
-        </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-text-field v-model="horse.father" :counter="64" :rules="length64" label="Vader" required outlined></v-text-field>
-          </v-col>
-          <v-col cols="12" md="4">
-            <v-text-field v-model="horse.mother" :counter="64" :rules="length64" label="Moeder" required outlined></v-text-field>
-          </v-col>
-          <v-col cols="12" md="4">
-            <v-text-field v-model="horse.grandfather" :counter="64" :rules="length64" label="Grootvader" required outlined></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row justify="end">
-          <v-btn v-if="!horse._id" :disabled="!valid" color="success" class="mr-4" @click="createHorse()" depressed>
-            Paard opslaan
-          </v-btn>
-          <v-btn v-if="horse._id" :disabled="!valid" color="success" depressed class="mr-4" @click="updateHorse()">
-            Paard bijwerken
-          </v-btn>
-          <v-btn v-if="horse._id" color="warning" depressed @click="deleteDialog = true">
-            Paard verwijderen
-          </v-btn>
-          <v-dialog v-model="deleteDialog" persistent max-width="350">
-            <v-card>
-              <v-card-title class="headline">Paard verwijderen?</v-card-title>
-              <v-card-text>Ben je zeker dat je het paard {{ horse.name }} wilt verwijderen? Dit kan niet meer ongedaan gemaakt worden</v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="success" depressed @click="deleteDialog = false">Annuleren</v-btn>
-                <v-btn color="error" depressed @click="deleteHorse()">Verwijderen</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-row>
-        <v-snackbar v-model="snackbar">
-          Paard is succesvol opgeslaan
-          <v-btn color="pink" text @click="snackbar = false">
-            sluiten
-          </v-btn>
-        </v-snackbar>
-      </v-container>
-    </v-form>
+    <v-tabs>
+      <v-tab>
+        <v-icon left>mdi-book-open-variant</v-icon>
+        Algemene gegevens
+      </v-tab>
+      <v-tab v-if="horse._id">
+        <v-icon left>mdi-passport</v-icon>
+        Paspoort
+      </v-tab>
+      <v-tab-item class="ma-5">
+        <v-card flat>
+          <v-form ref="form" v-model="valid">
+            <v-container>
+              <v-row>
+                <v-col cols="12" md="4">
+                  <v-text-field v-model="horse.name" :counter="64" :rules="nameRules" label="Naam paard" outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-select v-model="horse.type" :items="horseType" :rules="required" label="Geslacht" outlined></v-select>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <select-owner :owner="owner" @update-owner="updateOwner"></select-owner>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" md="4">
+                  <v-text-field v-model="horse.ueln" type="number" :counter="15" label="UELN" required outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-menu v-model="birthDateMenu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y min-width="290px">
+                    <template v-slot:activator="{ on }">
+                      <v-text-field v-model="computedBirthDateFormatted" label="Geboortedatum" v-on="on" readonly outlined></v-text-field>
+                    </template>
+                    <v-date-picker v-model="horse.date_of_birth" no-title @input="birthDateMenu = false"></v-date-picker>
+                  </v-menu>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" md="4" v-if="horse.type === 'merrie'">
+                  <v-switch v-model="horse.surrogate" label="Draagmoeder"></v-switch>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" md="4">
+                  <v-text-field v-model="horse.father" :counter="64" :rules="length64" label="Vader" required outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-text-field v-model="horse.mother" :counter="64" :rules="length64" label="Moeder" required outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-text-field v-model="horse.grandfather" :counter="64" :rules="length64" label="Grootvader" required outlined></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row justify="end">
+                <v-btn v-if="!horse._id" :disabled="!valid" color="success" class="mr-4" @click="createHorse()" depressed>
+                  Paard opslaan
+                </v-btn>
+                <v-btn v-if="horse._id" :disabled="!valid" color="success" depressed class="mr-4" @click="updateHorse()">
+                  Paard bijwerken
+                </v-btn>
+                <v-btn v-if="horse._id" color="warning" depressed @click="deleteDialog = true">
+                  Paard verwijderen
+                </v-btn>
+                <v-dialog v-model="deleteDialog" persistent max-width="350">
+                  <v-card>
+                    <v-card-title class="headline">Paard verwijderen?</v-card-title>
+                    <v-card-text>Ben je zeker dat je het paard {{ horse.name }} wilt verwijderen? Dit kan niet meer ongedaan gemaakt worden</v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="success" depressed @click="deleteDialog = false">Annuleren</v-btn>
+                      <v-btn color="error" depressed @click="deleteHorse()">Verwijderen</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-row>
+              <v-snackbar v-model="snackbar">
+                Paard is succesvol opgeslaan
+                <v-btn color="pink" text @click="snackbar = false">
+                  sluiten
+                </v-btn>
+              </v-snackbar>
+            </v-container>
+          </v-form>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item class="ma-5">
+        <v-card flat>
+          <horse-passport :horse="horse"></horse-passport>
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+
   </v-card>
 </template>
 
@@ -80,6 +102,7 @@
 import horseAPI from "@/services/HorseAPI.js";
 import customerAPI from "@/services/CustomerAPI.js";
 import selectOwner from "@/components/SelectOwner";
+import horsePassport from "@/components/horse/Passport";
 export default {
   props: ["id"],
   data: vm => ({
@@ -175,7 +198,8 @@ export default {
     }
   },
   components: {
-    selectOwner
+    selectOwner,
+    horsePassport
   },
 };
 </script>
