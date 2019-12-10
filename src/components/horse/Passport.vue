4913334684beb1bb1fb56c59fcdc3fca1151c7ac
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-10 21:47:35 +0100

    Updated component

--- src/components/horse/Passport.vue
+++ src/components/horse/Passport.vue
@@ -2,19 +2,31 @@
   <div>
     <v-form ref="form">
       <v-container>
-        <v-row>
-          <v-col cols="12" md="4">
-            <v-file-input v-model="file" show-size label="Paspoort"></v-file-input>
-          </v-col>
-        </v-row>
-        <v-row justify="end">
-          <v-btn color="success" class="mr-4" @click="submitPassport" depressed>
-            Passpoort uploaden
-          </v-btn>
-        </v-row>
+        <template v-if="!horse.passport">
+          <v-row>
+            <v-col cols="12" md="4">
+              <v-file-input v-model="file" show-size label="Paspoort"></v-file-input>
+            </v-col>
+          </v-row>
+          <v-row justify="end" v-if="file">
+            <v-btn color="success" class="mr-4" @click="createPassport" depressed>
+              Passpoort uploaden
+            </v-btn>
+          </v-row>
+        </template>
+        <template v-else>
+          <v-row>
+            <v-btn color="primary" class="ma-2 white--text" :href="passportLink" target="_blank">
+              Passpoort bekijken
+              <v-icon right dark>mdi-passport</v-icon>
+            </v-btn>
+            <v-btn color="warning" class="ma-2 white--text" @click="deletePassport()" target="_blank">
+              Passpoort verwijderen
+            </v-btn>
+          </v-row>
+        </template>
       </v-container>
     </v-form>
-    {{ horse._id }}
   </div>
 </template>
 <script>
@@ -23,20 +35,36 @@ export default {
   props: ['horse'],
   data() {
     return {
-      file: null
+      file: null,
+      baseURL: process.env.VUE_APP_API_BASE_URL
     };
   },
   methods: {
-    async submitPassport() {
+    async createPassport() {
       try {
         let formData = new FormData();
         formData.append('file', this.file);
-        //console.log(formData.file);
-        await horseAPI.postPassport(this.horse._id, formData);
+        const response = await horseAPI.postPassport(this.horse._id, formData);
+        console.log(response.data);
+        this.horse = response.data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async deletePassport() {
+      try {
+        const response = await horseAPI.deletePassport(this.horse);
+        console.log(response.data);
+        this.horse = response.data;
       } catch (e) {
         console.log(e);
       }
     }
+  },
+  computed: {
+    passportLink () {
+      return `${this.baseURL}/${this.horse.passport}`;
+    },
   }
 }
 </script>
