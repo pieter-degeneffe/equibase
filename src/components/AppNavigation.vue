91d6aec5fe54299e8ddaecf30f3eb27bedecc4fa
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-11 11:49:13 +0100

    Submenu for mares and stallions

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,8 +1,45 @@
 <template>
   <div>
     <v-navigation-drawer v-if="isLoggedIn()" v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
-      <v-list dense v-show="isLoggedIn()">
-        <v-list-item v-for="(item, i) in menu" :key="i" :to="item.url">
+      <v-list dense>
+        <template v-for="item in menu">
+          <v-list-group v-if="item.submenu" :key="item.text" :prepend-icon="item.icon" v-model="item.model">
+            <template v-slot:activator>
+              <v-list-item class="pl-0">
+                <v-list-item-content>
+                  <v-list-item-title>
+                    {{ item.text }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+            <v-list-item v-for="(child, i) in item.submenu" :key="i" :to="child.url">
+              <v-list-item-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ child.text }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item v-else :key="item.text" :to="item.url">
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ item.text }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- <v-navigation-drawer v-if="isLoggedIn()" v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <template v-for="(item, i) in menu">
+        <v-list-item v-if="!item.submenu" :key="i" :to="item.url">
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -12,14 +49,14 @@
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-      </v-list>
+      </template>
       <template v-slot:append>
         <div class="pa-2">
           <v-btn block v-show="isLoggedIn()" @click="handleLogout()">Logout</v-btn>
           <v-btn block v-show="!isLoggedIn()" @click="handleLogin()">Login</v-btn>
         </div>
       </template>
-    </v-navigation-drawer>
+    </v-navigation-drawer> -->
     <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-show="isLoggedIn()"></v-app-bar-nav-icon>
       <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
@@ -41,42 +78,29 @@ export default {
     return {
       drawer: true,
       menu: [
-        { text: "Klanten", url: "/customer", icon: "mdi-contacts" },
-        { text: "Paarden", url: "/horse", icon: "mdi-horseshoe"},
+        {
+          text: "Klanten",
+          url: "/customer",
+          icon: "mdi-contacts",
+        },
+        {
+          text: "Paarden",
+          icon: "mdi-horseshoe",
+          submenu: [
+            {
+              text: "Hengsten",
+              url: "/horse/stallion",
+              icon: "mdi-gender-male"
+            },
+            {
+              text: "Merries",
+              url: "/horse/mare",
+              icon: "mdi-gender-female"
+            }
+          ]
+        },
         // { text: 'Instellingen', url: "/settings", icon: 'mdi-settings'},
       ],
-      items: [
-        { icon: 'mdi-contacts', text: 'Contacts' },
-        { icon: 'mdi-history', text: 'Frequently contacted' },
-        { icon: 'mdi-content_copy', text: 'Duplicates' },
-        {
-          icon: 'mdi-keyboard_arrow_up',
-          'icon-alt': 'mdi-keyboard_arrow_down',
-          text: 'Labels',
-          model: true,
-          children: [
-            { icon: 'mdi-add', text: 'Create label' },
-          ],
-        },
-        {
-          icon: 'mdi-keyboard_arrow_up',
-          'icon-alt': 'mdi-keyboard_arrow_down',
-          text: 'More',
-          model: false,
-          children: [
-            { text: 'Import' },
-            { text: 'Export' },
-            { text: 'Print' },
-            { text: 'Undo changes' },
-            { text: 'Other contacts' },
-          ],
-        },
-      { icon: 'mdi-settings', text: 'Settings' },
-      { icon: 'mdi-chat_bubble', text: 'Send feedback' },
-      { icon: 'mdi-help', text: 'Help' },
-      { icon: 'mdi-phonelink', text: 'App downloads' },
-      { icon: 'mdi-keyboard', text: 'Go to the old version' },
-    ],
     };
   },
   methods: {
