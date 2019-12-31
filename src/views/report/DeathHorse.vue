1f96bf1818e3315e091e0a8eee1882cdb8626ed2
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2019-12-31 13:30:27 +0100

    Dynamic query paramters

--- src/views/report/DeathHorse.vue
+++ src/views/report/DeathHorse.vue
@@ -1,6 +1,12 @@
 <template>
   <v-card class="ma-5" outlined>
-    <v-data-table :headers="headers" :items="horses" :loading="loading">
+    <v-data-table :headers="headers" :items="horses" :loading="loading" class="ma-4">
+      <template v-slot:no-data>
+          Geen paarden gevonden
+      </template>
+      <template v-slot:top>
+        <v-toolbar-title class="mb-4">Overleden paarden</v-toolbar-title>
+      </template>
       <template v-slot:item="props">
         <tr @click="openHorsePage(props.item._id)" @mouseover="mouseOver(true)" @mouseleave="mouseOver(false)">
           <td>{{ props.item.name }}</td>
@@ -25,6 +31,9 @@ export default {
         { text: 'UELN', align: 'left', sortable: false, value: 'ueln' },
         { text: 'overlijdensdatum', align: 'right', sortable: false, value: 'date_of_death' }
       ],
+      URLParameters: {
+        'death': true
+      }
     };
   },
   created() {
@@ -33,7 +42,7 @@ export default {
   methods: {
     async loadHorses() {
       try {
-        const response = await horseAPI.getHorses(null, this.death);
+        const response = await horseAPI.getHorses(this.URLParameters);
         this.horses = response.data;
       } catch (e) {
         this.errored = true;
