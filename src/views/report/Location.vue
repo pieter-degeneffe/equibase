bcecf572b91e54531f410c705ad32718ede62bb6
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2020-01-17 15:46:23 +0100

    Addes stud horse filter

--- src/views/report/Location.vue
+++ src/views/report/Location.vue
@@ -7,32 +7,38 @@ export default {
   data () {
     return {
       headers: [
-        { text: 'Naam', value: 'name', align: 'left', sortable: false, selected: true },
+        { text: 'Naam', value: 'name', align: 'left', sortable: true, selected: true },
         { text: 'Brandnummer', value: 'surrogate_uid', align: 'left', sortable: false, selected: true },
-        { text: 'Microchip', value: 'microchip', align: 'left', selected: true},
         { text: 'Type', value: 'type', align: 'left', sortable: false, selected: false},
+        { text: 'Microchip', value: 'microchip', align: 'left', selected: true},
         { text: 'Locatie', value: 'location', align: 'left', sortable: true, selected: true},
-        { text: 'Eigenaar', value: 'owner', align: 'left', sortable: false, selected: false},
-        { text: 'Draagmoeder', value: 'surrogate', align: 'left', selected: true},
-
+        { text: 'Eigenaar', value: 'owner', align: 'left', sortable: true, selected: false},
+        { text: 'Draagmoeder', value: 'surrogate', align: 'left', selected: false},
         { text: 'Aangemaakt op', value: 'createdAt', selected: false },
-        { text: 'Gewijzigd op', value: 'updatedAt', sortable: false, selected: false },
+        { text: 'Gewijzigd op', value: 'updatedAt', sortable: true, selected: false},
         { text: 'Geboortedatum', value: 'date_of_birth', selected: false}
       ],
       filters: {
         type: {
           options: ["hengst", "merrie"],
-          value: null
+          value: "merrie"
         },
         surrogate: {
           value: true,
           label: null
         },
-        location: true,
+        stud_horse: {
+          value: false,
+          label: null
+        },
+        location: {
+          options: null,
+          value: true
+        },
         owner: null,
-        death: null
+        death: false
       },
-      sortBy: 'location.name'
+      sortBy: 'updatedAt'
     }
   },
   components: {
