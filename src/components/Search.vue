2b084d7f27de5a695dd6ec29e810370047c58f66
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2020-01-28 14:35:33 +0100

    Fixed indentation

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,37 +1,37 @@
 <template>
   <div>
-  <v-autocomplete
-    v-model="select"
-    :loading="loading"
-    :items="items"
-    :search-input.sync="search"
-    prepend-inner-icon="mdi-magnify"
-    flat
-    dense
-    hide-no-data
-    hide-details
-    hide-selected
-    item-text="Description"
-    item-value="_id"
-    label="Zoeken"
-    autocomplete="off"
-    solo-inverted
-    return-object
-    no-filter
-  >
-  <template v-slot:item="{ item }">
-    <v-list-item @click="openPage(item.url)">
-      <v-list-item-action>
-        <v-icon>{{ item.icon }}</v-icon>
-      </v-list-item-action>
-      <v-list-item-content>
-        <v-list-item-title>{{ item.Description }}</v-list-item-title>
-        <v-list-item-subtitle>{{ item.company }}</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-  </template>
-  </v-autocomplete>
-</div>
+    <v-autocomplete
+      v-model="select"
+      :loading="loading"
+      :items="items"
+      :search-input.sync="search"
+      prepend-inner-icon="mdi-magnify"
+      flat
+      dense
+      hide-no-data
+      hide-details
+      hide-selected
+      item-text="Description"
+      item-value="_id"
+      label="Zoeken"
+      autocomplete="off"
+      solo-inverted
+      return-object
+      no-filter
+    >
+    <template v-slot:item="{ item }">
+      <v-list-item @click="openPage(item.url)">
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ item.Description }}</v-list-item-title>
+          <v-list-item-subtitle>{{ item.company }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </template>
+    </v-autocomplete>
+  </div>
 </template>
 <script>
 import searchAPI from "@/services/SearchAPI.js";
@@ -82,7 +82,6 @@ export default {
         console.log(err);
       } finally {
         this.loading = false;
-        console.log(this.entries);
       }
     },
     openPage(url){
