2869c694dcf7711ba8e0eabadd7985449e37c1fa
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2020-02-04 15:19:22 +0100

    New component for semencollection detail

---
+++ src/components/horse/semenCollection/EditDialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-dialog v-model="editDialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Details van het lot</span>
+        </v-card-title>
+        <v-card-text>
+          {{ semenCollection }}
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script>
+export default {
+  props: ['semenCollection','editDialog']
+}
+</script>
