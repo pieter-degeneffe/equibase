11265372090cdc80256ab03d3b3227c02b504f7a
Pieter Degeneffe <pieterdegeneffe@Pieters-MacBook-Pro.local>
2020-02-12 08:45:13 +0100

    Refactoring

--- src/components/horse/semenCollection/Table.vue
+++ src/components/horse/semenCollection/Table.vue
@@ -4,7 +4,7 @@
       <template v-slot:top>
         <v-toolbar flat color="white">
           <v-spacer></v-spacer>
-          <create-dialog :horse="horse" @created-semen-collection="addSemenCollection"></create-dialog>
+          <v-btn color="primary" dark  @click="openCreateDialog()" class="d-print-none">Sperma lot toevoegen</v-btn>
         </v-toolbar>
       </template>
       <template v-slot:no-data>
@@ -22,14 +22,38 @@
             {{ props.item.location.container.name }} - Koker {{ props.item.location.tube }} - {{ props.item.location.position }}
           </td>
           <td align='right' class="d-print-none">
-            <v-icon small @click="openDetailDialog(props.item)" class="mr-2">mdi-magnify</v-icon>
-            <v-icon small @click="openEditDialog(props.item)">mdi-pencil</v-icon>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-icon dark color="primary" @click="openDialog('create', props.item)" v-on="on" class="mr-2">mdi-pencil</v-icon>
+              </template>
+              <span>Lot bewerken</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-icon dark color="primary" @click="openDialog('edit', props.item)" v-on="on" class="mr-2">mdi-plus</v-icon>
+              </template>
+              <span>Stockwijzigingen toevoegen</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-icon dark color="primary" @click="openDialog('detail', props.item)" v-on="on" class="mr-2">mdi-magnify</v-icon>
+              </template>
+              <span>Bekijk stockwijzigingen</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-icon dark color="primary" @click="openDialog('delete', props.item)" v-on="on">mdi-delete</v-icon>
+              </template>
+              <span>Lot verwijderen</span>
+            </v-tooltip>
           </td>
         </tr>
       </template>
     </v-data-table>
-    <edit-dialog v-if="semenCollection" @update-semen-collection="updateSemenCollection" :semenCollection="semenCollection" :editDialog="editDialog" @close-edit-dialog="closeEditDialog"></edit-dialog>
-    <detail-dialog v-if="semenCollection" :semenCollection="semenCollection" :detailDialog="detailDialog" @close-edit-dialog="closeDetailDialog"></detail-dialog>
+    <create-dialog v-if="dialogs.create" :horse="horse" :semenCollection="semenCollection" :createDialog="dialogs.create" @created-semen-collection="addSemenCollection" @update-semen-collection="updateSemenCollection" @close-dialog="dialogs.create = false"></create-dialog>
+    <edit-dialog v-if="semenCollection" @update-semen-collection="updateSemenCollection" :semenCollection="semenCollection" :editDialog="dialogs.edit" @close-dialog="dialogs.edit = false"></edit-dialog>
+    <detail-dialog v-if="semenCollection" :semenCollection="semenCollection" :detailDialog="dialogs.detail" @close-dialog="dialogs.detail = false"></detail-dialog>
+    <delete-dialog v-if="semenCollection" @delete-semen-collection="deleteSemenCollection" :semenCollection="semenCollection" :deleteDialog="dialogs.delete" @close-dialog="dialogs.delete = false"></delete-dialog>
     <v-alert type="error" v-if="errored" >
       {{ errorMessage }}
     </v-alert>
@@ -40,20 +64,20 @@ import semenAPI from "@/services/SemenAPI.js";
 import editDialog from "@/components/horse/semenCollection/EditDialog";
 import createDialog from "@/components/horse/semenCollection/CreateDialog";
 import detailDialog from "@/components/horse/semenCollection/DetailDialog";
+import deleteDialog from "@/components/horse/semenCollection/DeleteDialog";
 export default {
   props: ['horse'],
   data() {
     return {
-      editDialog: false,
-      detailDialog: false,
+      dialogs: { create: false, edit: false, detail: false, delete: false },
       headers: [
         { text: 'Hengst', align: 'left', sortable: false},
-        { text: 'Eigenaar'},
-        { text: 'Beginwaarde'},
-        { text: 'Beschikbaar'},
+        { text: 'Eigenaar sperma lot'},
+        { text: 'Start stock'},
+        { text: 'Eind stock'},
         { text: 'Productiedatum'},
         { text: 'locatie & kleur'},
-        { text: 'Bewerken', align: 'right', value: 'action', sortable: false, class: "d-print-none"}
+        { text: '', align: 'right', value: 'action', sortable: false, class: "d-print-none"}
       ],
       loading: false,
       semenCollections: [],
@@ -90,51 +114,36 @@ export default {
     },
     updateSemenCollection(semenCollection) {
       const index = this.semenCollections.findIndex(x => x._id === semenCollection._id);
-      console.log(semenCollection.modifications);
-      this.semenCollections[index].modifications = semenCollection.modifications;
-      this.semenCollections[index].current_inventory = semenCollection.current_inventory;
+      this.semenCollections.splice(index, 1, semenCollection);
     },
-    openEditDialog(semenCollection) {
-      this.editDialog = true;
+    deleteSemenCollection(semenCollection) {
+      const index = this.semenCollections.findIndex(x => x._id === semenCollection._id);
+      this.semenCollections.splice(index, 1);
+    },
+    openDialog(dialogType, semenCollection) {
+      this.dialogs[dialogType] = true;
       this.semenCollection = semenCollection;
-    },
-    openDetailDialog(semenCollection) {
-      this.detailDialog = true;
-      this.semenCollection = semenCollection;
-    },
-    closeDetailDialog(){
-      this.detailDialog = false;
-    },
-    closeEditDialog(){
-      this.editDialog = false;
     },
     ownerName(owner){
       return owner.type === "particulier" ? `${owner.first_name} ${owner.last_name}` : `${owner.company}`
     },
     strawColor(color) {
-      //return 'red--text';
       switch(color) {
-        case 'Rood':
-          return "red--text";
-        case 'Oranje':
-          return "orange--text";
-        case 'Geel':
-          return "yellow--text";
-        case 'Groen':
-          return "green--text";
-        case 'Blauw':
-          return "blue--text";
-        case 'Indigo':
-          return "indigo--text";
-        case 'Violet':
-          return "purple-color";
+        case 'Rood': return "red--text";
+        case 'Oranje': return "orange--text";
+        case 'Geel': return "yellow--text";
+        case 'Groen': return "green--text";
+        case 'Blauw': return "blue--text";
+        case 'Indigo': return "indigo--text";
+        case 'Violet': return "purple-color";
       }
     }
   },
   components: {
     editDialog,
     createDialog,
-    detailDialog
+    detailDialog,
+    deleteDialog
   }
 }
 </script>
