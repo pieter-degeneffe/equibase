1e64154ce322dc0795b615e0bab22657f556eb47
jens.verc <verc.jens@gmail.com>
2020-09-02 13:46:29 +0200

    add display for products & error row

--- src/views/settings/Products.vue
+++ src/views/settings/Products.vue
@@ -1,6 +1,15 @@
 <template>
   <v-card class="ma-5" outlined>
+    <v-data-table :headers="headers" :items="products" :loading="loading" loading-text="Bezig met laden...">
 
+    </v-data-table>
+    <v-row>
+      <v-col cols="12">
+        <v-alert type="error" v-if="errored" class="mx-5">
+          {{ errorMessage }}
+        </v-alert>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
@@ -12,6 +21,19 @@ export default {
       loading: null,
       errored: false,
       errorMessage: '',
+      headers: [
+        { text: 'Naam medicijn', align: 'left', sortable: true, value: 'name' },
+        { text: 'Type', align: 'left', sortable: true, value: 'type' },
+        { text: 'CNK', align: 'left', sortable: true, value: 'CNK' },
+        { text: 'Eenheid', align: 'left', sortable: false, value: 'outgoingUnit' },
+        { text: 'BTW', align: 'left', sortable: false, value: 'tax' },
+        { text: 'Verkoopsprijs', align: 'left', sortable: false, value: 'sellingPrice' },
+        { text: 'Verkoopsprijs/eenheid', align: 'left', sortable: false, value: 'sellingPricePerUnit' },
+        { text: 'supplementAdministration', align: 'left', sortable: false, value: 'supplementAdministration' },
+        { text: 'waitingTime', align: 'left', sortable: false, value: 'waitingTime' },
+        { text: 'unitSellingPrice', align: 'left', sortable: false, value: 'unitSellingPrice' },
+        { text: 'unitAdministrationPrice', align: 'left', sortable: false, value: 'unitAdministrationPrice' },
+      ],
       products: [],
     }
   },
@@ -23,8 +45,8 @@ export default {
       this.loading = true;
       try {
         const response = await productsAPI.getProduct();
-        this.products = response.data;
-        console.table(response.data.products);
+        this.products = response.data.products;
+        //console.table(response.data.products);
       } catch (e) {
         console.log(e);
         this.errored = true;
