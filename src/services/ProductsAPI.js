88c2f746b989c28bacf90d86105e15dbd5607c2c
Arne Herbots <45999127+aHerbots@users.noreply.github.com>
2020-09-07 14:04:38 +0200

    Update src/services/ProductsAPI.js

--- src/services/ProductsAPI.js
+++ src/services/ProductsAPI.js
@@ -5,6 +5,6 @@ export default {
     getProduct: (id) => API().get(`/api/product/${ id }`),
     getConfig: () => API().get('/api/product/config'),
     putProduct: (product) => API().put(`/api/product/${ product._id }`, { product }),
-    deleteProduct: (id) => API().delete('/api/product/' + id),
+    deleteProduct: (id) => API().delete(`/api/product/${id}`),
     searchProduct: (searchValue) => API().get( `/api/product/${ searchValue }`)
 };
