a85cfd55f890da96a57511398c4bf673d1f90fef
jens.verc <verc.jens@gmail.com>
2020-09-08 13:41:10 +0200

    Add getStockProduct .get

--- src/services/StockAPI.js
+++ src/services/StockAPI.js
@@ -1,4 +1,5 @@
 import API from "@/services/API";
 export default {
     getAllStock: (parameters) => API().get('/api/stock', { params: parameters }),
+    getStockProduct: (id) => API().get(`/api/stock/${ id }`),
 };
