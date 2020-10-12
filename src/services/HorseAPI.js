2c1ce78bdd97c27c297e413a65d52961851592df
Arne Herbots <45999127+aHerbots@users.noreply.github.com>
2020-10-12 16:04:55 +0200

    Update src/services/HorseAPI.js

--- src/services/HorseAPI.js
+++ src/services/HorseAPI.js
@@ -6,7 +6,7 @@ export default {
   getHorse: (id) => API().get(`/api/horse/${id}`),
   putHorse: (horse) => API().put(`/api/horse/${horse._id}`, {horse}),
   deleteHorse: (id) => API().delete(`/api/horse/${id}`),
-  postPassport: (id, formData) => API().post(`/api/horse/${id}/passport`, formData,{headers: {'Content-Type': 'multipart/form-data'}}),
+  postPassport: (id, formData) => API().post(`/api/horse/${id}/passport`, formData, {headers: {'Content-Type': 'multipart/form-data'}}),
   deletePassport: (horse) => API().delete(`/api/horse/${horse._id}/passport`),
   postLodging: (horse) => API().post(`/api/horse/${horse._id}/lodging`, {horse}),
   deleteLodging: (horse, lodging) => API().delete(`/api/horse/${horse._id}/lodging/${lodging._id}`)
