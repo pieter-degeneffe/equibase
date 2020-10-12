56b892cceca9a3d05d291e5f144f2f1362bf76cf
jens.verc <verc.jens@gmail.com>
2020-10-12 16:10:14 +0200

    arrow functions & template string -ify

--- src/services/SemenAPI.js
+++ src/services/SemenAPI.js
@@ -1,29 +1,9 @@
 import API from "@/services/API";
 export default {
-  postSemenCollection(semenCollection) {
-    return API().post("/api/semen", {
-      semenCollection
-    });
-  },
-  getSemenCollection(parameters) {
-    return API().get("/api/semen", {
-      params: parameters
-    });
-  },
-  putSemenCollection(semenCollection) {
-    return API().put("/api/semen/" + semenCollection._id, {
-      semenCollection
-    });
-  },
-  deleteSemenCollection(semenCollection) {
-    return API().delete("/api/semen/" + semenCollection._id);
-  },
-  postSemenCollectionModification(semenCollection, semenCollectionModification) {
-    return API().post("/api/semen/" + semenCollection._id + "/modification", {
-      semenCollectionModification
-    });
-  },
-  deleteSemenCollectionModification(semenCollection, semenCollectionModification) {
-    return API().delete("/api/semen/" + semenCollection._id + "/modification/" + semenCollectionModification._id);
-  },
+  postSemenCollection: (semenCol) => API().post("/api/semen", {semenCol}),
+  getSemenCollection: (params) => API().get("/api/semen", {params}),
+  putSemenCollection: (semenCol) => API().put(`/api/semen/${semenCol._id}`, {semenCol}),
+  deleteSemenCollection: (semenCol) => API().delete(`/api/semen/${semenCol._id}`),
+  postSemenCollectionModification: (semenCol, semenColMods) => API().post(`/api/semen/${semenCol._id}/modification`, { semenColMods}),
+  deleteSemenCollectionModification: (semenCol, semenColMods) => API().delete(`/api/semen/${semenCol._id}/modification/${semenColMods._id}`),
 };
