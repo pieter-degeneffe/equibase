754bcbcee5f1f0267bf1533402a2eacb2926358b
jens.verc <verc.jens@gmail.com>
2020-10-12 16:04:54 +0200

    arrow functions & template string -ify

--- src/services/LocationAPI.js
+++ src/services/LocationAPI.js
@@ -1,22 +1,8 @@
 import API from "@/services/API";
 export default {
-  postLocation(location) {
-    return API().post("/api/location", {
-      location
-    });
-  },
-  getLocations() {
-    return API().get("/api/location");
-  },
-  getLocation(id) {
-    return API().get("/api/location/" + id);
-  },
-  putLocation(location) {
-    return API().put("/api/location/" + location._id, {
-      location
-    });
-  },
-  deleteLocation(id) {
-    return API().delete("/api/location/" + id);
-  }
+  postLocation: (location) => API().post("/api/location", {location}),
+  getLocations: () => API().get("/api/location"),
+  getLocation: (id) => API().get(`/api/location/${id}`),
+  putLocation: (location) => API().put(`/api/location/${location._id}`, {location}),
+  deleteLocation: (id) => API().delete(`/api/location/${id}`),
 };
