8ff80227bebf5852e39bfe3669c07bcdf6cfd9e6
jens.verc <verc.jens@gmail.com>
2020-10-12 15:51:55 +0200

    resolve warning

--- src/components/stock/Mods.vue
+++ src/components/stock/Mods.vue
@@ -93,7 +93,7 @@
           <td v-if="outgoing && showColumn('client')">{{ props.item.client ? `${props.item.client.last_name} ${props.item.client.first_name}` : '-' }}</td>
           <td v-if="(preFilter === 'Toediening') || outgoing && showColumn('horse')">{{ props.item.horse ? props.item.horse.name : '-' }}</td>
           <td v-if="showColumn('amount')">{{ props.item.amount }}</td>
-          <td v-if="showColumn('createdAt')" align="end">{{ formatDate(props.item.createdAt) }}</td>
+          <td v-if="showColumn('createdAt')" class="text-right">{{ formatDate(props.item.createdAt) }}</td>
         </tr>
       </template>
     </v-data-table>
